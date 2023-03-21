4f7deead3f594dcac800afdb1298f75b433440a0
douglas.morais@i-value.com.br <douglas.morais@i-value.com.br>
2023-03-21 19:59:58 -0300

    update css

--- src/App.js
+++ src/App.js
@@ -3,17 +3,36 @@ import React from 'react'
 import Login from './components/login';
 import Profile from './components/profile';
 import Footer from './components/footer';
-import { useAuth } from './providers/auth'
+import { useAuth } from './providers/auth';
+import styles from './style.module.css'
 
 function App() {
   const { user } = useAuth()
   console.log(user)
 
+  const [ isLogin, setIsLogin ] = React.useState(false);
+
+  const handleDialogLogin = () => {
+    setIsLogin(!isLogin)
+  }
+
   return (
     <>
       <h1>Olá GamaAcademy</h1>
       <Profile />
-      <Login />
+      { !isLogin && (
+        <div>
+          <button onClick={handleDialogLogin}>Deseja logar?</button>
+        </div>
+      )}
+      { isLogin && (
+        <>
+          <div className={styles.box}>
+            <button onClick={handleDialogLogin}>Fechar</button>
+            <Login />
+          </div>
+        </>
+      )}
       <Footer />
     </>
   );
